9ff482e390087d7f56e44810171f9aa839c0e3f0
Filip <filipsarovic428@gmail.com>
2023-03-02 03:39:26 +0100

    added slash in the end of client path

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch('https://the-botfather-chatgpt.onrender.com', {
+  const response = await fetch('https://the-botfather-chatgpt.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
