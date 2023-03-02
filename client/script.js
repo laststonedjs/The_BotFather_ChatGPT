15b2f471cd111b8b7b4098e0927a461a8c03baf3
Filip <filipsarovic428@gmail.com>
2023-03-02 13:35:38 +0100

    updated the server gitignore

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response https://the-botfather-chatgpt.onrender.com
-  const response = await fetch('https://botfather.onrender.com/', {
+  const response = await fetch('https://botfather.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
