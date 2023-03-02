257df567997e404ba5df7e860e6e35bcc748c024
Filip <filipsarovic428@gmail.com>
2023-03-02 02:33:35 +0100

    Corrections with prod path

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch('http://botfather.onrender.com', {
+  const response = await fetch('https://botfather.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
