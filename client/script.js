5e9bfff78f74e01f81212812a240de6f62b909a1
Filip <filipsarovic428@gmail.com>
2023-03-02 13:31:54 +0100

    updated the server url

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response https://the-botfather-chatgpt.onrender.com
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://botfather.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
